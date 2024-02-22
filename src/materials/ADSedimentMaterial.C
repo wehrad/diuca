6a1eed8b43a806c939397ee7cbd17423e69e6670
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-02-22 12:45:41 -0700

    Reorder constructor for Sediment material Code format

--- src/materials/ADSedimentMaterial.C
+++ src/materials/ADSedimentMaterial.C
@@ -18,12 +18,12 @@ ADSedimentMaterial::validParams()
 
   // Solid properties
   // https://tc.copernicus.org/articles/14/261/2020/
-  params.addParam<ADReal>("density", 1850., "Sediment density"); // kgm-3
+  params.addParam<ADReal>("density", 1850., "Sediment density");                  // kgm-3
   params.addParam<ADReal>("II_eps_min", 6.17e-6, "Finite strain rate parameter"); // a-1
-  
+
   // Friction properties
   params.addParam<ADReal>("FrictionCoefficient", 1.0, "Sediment friction coefficient");
-  
+
   return params;
 }
 
@@ -32,12 +32,9 @@ ADSedimentMaterial::ADSedimentMaterial(const InputParameters & parameters)
 
     // Mesh dimension
     _mesh_dimension(_mesh.dimension()),
-    
+
     // Sediment density
     _rho(getParam<ADReal>("density")),
-
-    // Friction properties
-    _FrictionCoefficient(getParam<ADReal>("FrictionCoefficient")),
 
     // Velocity gradients
     _grad_velocity_x(adCoupledGradient("velocity_x")),
@@ -46,13 +43,16 @@ ADSedimentMaterial::ADSedimentMaterial(const InputParameters & parameters)
 
     // Finite strain rate parameter
     _II_eps_min(getParam<ADReal>("II_eps_min")),
-    
+
     // Mean stress
     _pressure(adCoupledValue("pressure")),
 
+    // Friction properties
+    _FrictionCoefficient(getParam<ADReal>("FrictionCoefficient")),
+
     // Ice properties created by this object
-    _viscosity(declareADProperty<Real>("mu")),
-    _density(declareADProperty<Real>("rho"))
+    _density(declareADProperty<Real>("rho")),
+    _viscosity(declareADProperty<Real>("mu"))
 {
 }
 
@@ -67,7 +67,7 @@ ADSedimentMaterial::computeQpProperties()
   ADReal u_x = _grad_velocity_x[_qp](0);
   ADReal u_y = _grad_velocity_x[_qp](1);
   ADReal u_z = _grad_velocity_x[_qp](2);
-  
+
   ADReal v_x = _grad_velocity_y[_qp](0);
   ADReal v_y = _grad_velocity_y[_qp](1);
   ADReal v_z = _grad_velocity_y[_qp](2);
@@ -76,20 +76,20 @@ ADSedimentMaterial::computeQpProperties()
   ADReal w_y = _grad_velocity_z[_qp](1);
   ADReal w_z = _grad_velocity_z[_qp](2);
 
-  ADReal eps_xy = 0.5 * (u_y + v_x);                                                            
+  ADReal eps_xy = 0.5 * (u_y + v_x);
   ADReal eps_xz = 0.5 * (u_z + w_x);
-  ADReal eps_yz = 0.5 * (v_z + w_y); 
+  ADReal eps_yz = 0.5 * (v_z + w_y);
 
   // Get pressure
   ADReal sig_m = _pressure[_qp];
-  
+
   ADReal eta = _viscosity[_qp];
 
   // Compute stresses
   ADReal sxx = 2 * eta * u_x + sig_m;
   ADReal syy = 2 * eta * v_y + sig_m;
   ADReal szz = 2 * eta * w_z + sig_m;
-  
+
   ADReal sxy = eta * (u_y + v_x);
   ADReal sxz = eta * (u_z + w_x);
   ADReal syz = eta * (v_z + w_y);
@@ -98,14 +98,13 @@ ADSedimentMaterial::computeQpProperties()
   ADReal sxx_dev = 2 * eta * u_x;
   ADReal syy_dev = 2 * eta * v_y;
   ADReal szz_dev = 2 * eta * w_z;
- 
+
   // von Mises stress (second invariant)
-  ADReal sig_e = std::sqrt(3./2. * (sxx_dev*sxx_dev + syy_dev*syy_dev + 2*sxy*sxy));
-  
+  ADReal sig_e = std::sqrt(3. / 2. * (sxx_dev * sxx_dev + syy_dev * syy_dev + 2 * sxy * sxy));
+
   // Compute viscosity
   // _viscosity[_qp] = (_FrictionCoefficient * sig_m) / std::abs(sig_e); // MPaa
   _viscosity[_qp] = 3.;
 
   // std::cout << "SEDIMENT  " << _viscosity[_qp] << "  " << _pressure[_qp] << std::endl;
-  
 }
