25fbe175682aaa67acbe7ac03a340f741e5220b3
AdrienWehrle <adrien.wehrle@hotmail.fr>
2024-02-26 15:59:14 +0100

    add FVSedimentMaterialSI

---
+++ include/materials/FVSedimentMaterialSI.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "FunctorMaterial.h"
+
+/**
+ * Material objects inherit from Material and override computeQpProperties.
+ *
+ * Their job is to declare properties for use by other objects in the
+ * calculation such as Kernels and BoundaryConditions.
+ */
+class FVSedimentMaterialSI : public FunctorMaterial
+{
+public:
+  static InputParameters validParams();
+
+  FVSedimentMaterialSI(const InputParameters & parameters);
+
+protected:
+  const unsigned int _mesh_dimension;
+
+  // Sediment friction parameter
+  const ADReal _FrictionParameter;
+
+  // density of the fluid
+  const ADReal & _rho;
+
+  // velocity
+  const Moose::Functor<ADReal> & _vel_x;
+  const Moose::Functor<ADReal> & _vel_y;
+  const Moose::Functor<ADReal> & _vel_z;
+
+  // pressure
+  const ADVariableValue & _pressure;
+
+};
