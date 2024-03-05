3d25c1d5fbbf6b429b42bc899d03095f2844879f
AdrienWehrle <adrien.wehrle@hotmail.fr>
2024-03-05 18:50:43 +0100

    bring MASTODON files for seismic analysis

---
+++ include/base/MastodonTypes.h
@@ -0,0 +1,24 @@
+/*************************************************/
+/*           DO NOT MODIFY THIS HEADER           */
+/*                                               */
+/*                     MASTODON                  */
+/*                                               */
+/*    (c) 2015 Battelle Energy Alliance, LLC     */
+/*            ALL RIGHTS RESERVED                */
+/*                                               */
+/*   Prepared by Battelle Energy Alliance, LLC   */
+/*     With the U. S. Department of Energy       */
+/*                                               */
+/*     See COPYRIGHT for full restrictions       */
+/*************************************************/
+
+// STL includes
+#include <limits>
+
+// Defines a Mastodon namespace in which special types and enums should be
+// defined.
+namespace Mastodon
+{
+/// Invalid layer id (see LayeredMaterialInterface)
+extern const unsigned int INVALID_LAYER_ID;
+}
